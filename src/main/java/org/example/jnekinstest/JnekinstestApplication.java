0d7a49cf09a098af0339b4e53870fb3b6df9656f
Stanimir <stanimir.slavov@helloteam.com>
2026-03-12 16:14:01 +0200

    .

---
+++ src/main/java/org/example/jnekinstest/JnekinstestApplication.java
@@ -0,0 +1,13 @@
+package org.example.jnekinstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JnekinstestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JnekinstestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/jnekinstest/JnekinstestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jnekinstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JnekinstestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
